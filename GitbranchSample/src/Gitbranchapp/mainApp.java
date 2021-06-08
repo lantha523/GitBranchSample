c76a72507606963be6f73c7b870a31b89cec43e9
AshrafChu <c10565021@gmail.com>
2021-06-08 16:24:31 +0800

    add second version

--- GitbranchSample/src/Gitbranchapp/mainApp.java
+++ GitbranchSample/src/Gitbranchapp/mainApp.java
@@ -2,8 +2,18 @@ package Gitbranchapp;
 
 public class mainApp {
 	//這是第一次測試版本
-
-	
 	
 	//second version
+
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		
+		
+		
+		int fdd = (int) Math.random()*10;
+		
+		
+		System.out.println(fdd);
+	}
 }
+	
