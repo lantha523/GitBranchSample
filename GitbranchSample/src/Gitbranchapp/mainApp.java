f6febbab38e04eca49c429542b396d087f1e42c0
lianghung523 <lianghung523@gmail.com>
2021-06-08 16:05:51 +0800

    2nd version

--- GitbranchSample/src/Gitbranchapp/mainApp.java
+++ GitbranchSample/src/Gitbranchapp/mainApp.java
@@ -1,6 +1,18 @@
 package Gitbranchapp;
 
 public class mainApp {
+	public static void main(String[] args) {
+		
+	
 	//這是第一次測試版本
-
+    Math cmath = new Math();
+    System.out.println();
+    System.out.println(cmath.add(10,20));
+    
+	}
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
