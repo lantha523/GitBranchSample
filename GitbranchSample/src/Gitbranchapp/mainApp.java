595184344bbba2f7f588797a3fecd96c8da3b575
AshrafChu <c10565021@gmail.com>
2021-06-14 12:49:34 +0800

    Change with comment on mainApp.java, with Intellij Idea

--- GitbranchSample/src/Gitbranchapp/mainApp.java
+++ GitbranchSample/src/Gitbranchapp/mainApp.java
@@ -4,4 +4,5 @@ public class mainApp {
 	//這是第一次測試版本
     //changed by ashraf via Intellij IDEA ultimate edition @MacOS Big Sur
 	//回傳給你 第二次測試
+    //Fetch from GitHub succeeded, saw your update  "//回傳給你 第二次測試";
 }
